8ecb894215034b9d1c6a4e3442ced0066a48e667
aff49866 <z498660443@gmail.com>
2017-09-15 00:29:42 +0800

    福利吧

--- fuliba.py
+++ fuliba.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import re
 # import os
 # os.environ['NO_PROXY'] = 'fuliba.net' #不使用代理
+def geturl(all_link,url):
+    html = gethtml(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    url_link_list = soup.find_all(rel="bookmark")
+    for a in url_link_list:
+        link = a.attrs["href"]
+        all_link.append([link])
 def gethtml(url):
     try:
         r = requests.get(url)
@@ -11,17 +18,29 @@ def gethtml(url):
         return r.text
     except ZeroDivisionError as e:
         raise ValueError(e)
-def getimg(ulist,html):
-    imglist = re.findall(r'http://[\w\d]*?.sinaimg.cn/[\w\d]*?/[\w\d]*?.jpg', html)
-    print(imglist)
-    soup = BeautifulSoup(html,'html.parser')
+def getimg(ulist,html,soup):
+    # imglist = re.findall(r'http://[\w\d]*?.sinaimg.cn/[\w\d]*?/[\w\d]*?.jpg', html)
+    # print(imglist)
     # imgd3 = soup.find_all(title='煎蛋年度妹子图TOP1000')
-    for i in soup.find_all(title='煎蛋年度妹子图TOP1000'):
-        img = i.attrs['src']
-        print(img)
-        ulist.append(img)
-def main():
-    html = gethtml('http://fuliba.net/jd-image-top1000.html')
+    for i in soup.find_all("p"):
+        if i.img != None:
+            img = i.img['src']
+            ulist.append(img)
+def printimg(ulist):
+    for u in ulist:
+        print(u)
+def main(depth_num):
+    depth = depth_num
+    secend_url_list = []
+    for l in range(depth):
+        url = "http://fuliba.net/page/"
+        url = url + str(l+1)
+        geturl(secend_url_list,url)
+    # print(secend_url_list)
     ulist = []
-    getimg(ulist,html)
-main()
+    for n in secend_url_list:
+        html = gethtml(n[0])
+        soup = BeautifulSoup(html, 'html.parser')
+        getimg(ulist, html, soup)
+    printimg(ulist)
+main(3)
