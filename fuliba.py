2781958c78e985c624eda899a64e5d7184bc8e3a
aff49866 <z498660443@gmail.com>
2017-09-15 17:46:52 +0800

    福利吧更新(多线程、图片存储)

--- fuliba.py
+++ fuliba.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-# import os
+import time
+import threading
+import os
 # os.environ['NO_PROXY'] = 'fuliba.net' #不使用代理
-def geturl(all_link,url):
+def geturl(all_link,url): #获取url
     html = gethtml(url)
     soup = BeautifulSoup(html, 'html.parser')
     url_link_list = soup.find_all(rel="bookmark")
     for a in url_link_list:
         link = a.attrs["href"]
         all_link.append([link])
-def gethtml(url):
+def gethtml(url): #获取html
     try:
         r = requests.get(url)
         r.raise_for_status()
@@ -18,25 +19,41 @@ def gethtml(url):
         return r.text
     except ZeroDivisionError as e:
         raise ValueError(e)
-def getimg(ulist,soup):
+def getimg(ulist,soup): #获取图片
     for i in soup.find_all("p"):
         if i.img != None:
             img = i.img['src']
             ulist.append(img)
-def printimg(ulist):
-    for u in ulist:
-        print(u)
+def saveimg(ulist):
+    root = "I://python//pics//"
+    for url in ulist:
+        path = root + url.split("/")[-1]
+        try:
+            if not os.path.exists(path):
+                imgr = requests.get(url)
+                with open(path,'wb') as f:
+                    f.write(imgr.content)
+            else:
+                print("文件已经存在")
+        except ZeroDivisionError as e:
+            raise ValueError(e)
 def main(depth_num):
     depth = depth_num
     secend_url_list = []
+    ulist = []
+    start_time = time.time()
     for l in range(depth):
         url = "http://fuliba.net/page/"
         url = url + str(l+1)
-        geturl(secend_url_list,url)
-    ulist = []
+        thread = threading.Thread(target=geturl(secend_url_list,url), )
+        thread.start()
+        thread.join()
     for n in secend_url_list:
         html = gethtml(n[0])
         soup = BeautifulSoup(html, 'html.parser')
-        getimg(ulist,h,soup)
-    printimg(ulist)
-main(3)
+        thread2 = threading.Thread(target=getimg(ulist,soup), )
+        thread2.start()
+        thread2.join()
+    saveimg(ulist)
+    print("time", time.time() - start_time)
+main(1)
