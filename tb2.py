1dfc3c314b1db7f751e0078bc7dc8870fb1d437f
aff49866 <z498660443@gmail.com>
2017-09-19 16:05:02 +0800

    随机生成N个数，它们之和等于M。并且每个数不小于某个值X

--- tb2.py
+++ tb2.py
@@ -22,9 +22,26 @@
 # # html2 = urllib.parse.quote(html) #编码
 # # print(html)
 # # print(html2)
+
+# import random
+# def totle(totle):
+#     a = random.randint(1000,5000)
+#     b = totle - a
+#     print(totle,a,b)
+# totle(5000)
+
 import random
-def totle(totle):
-    a = random.randint(1000,5000)
-    b = totle - a
-    print(totle,a,b)
-totle(5000)
+def constrained_sum_sample_pos(n, total):
+    '''随机生成N个数，它们之和等于M。先随机生成N-1个数的列表list，从小到大排序，然后加入最小数0产生list2，加入最大数M产生list3，然后将列表中对应的项相减。生成最终的列表。 举例：随机产生[1,2,3,4]
+那么如果最终之和是10，[1,2,3,4,10]减去[0,1,2,3,4]，得到新的列表[1,1,1,1,6]，然后各个数相加就等同于(1-0)+(2-1)+(3-2)+(4-3)+(10-4)=(1+2+3+4+10)-1-2-3-4-0=10-0=10，中间随机产生的数都抵销掉
+然后可限定最终列表的最小值min(list)
+'''
+    dividers = sorted(random.sample(range(1, total), n - 1))
+    num_list=[]
+    for a, b in zip(dividers + [total], [0] + dividers):
+        num_list.append(a-b)
+    return num_list
+list2 = constrained_sum_sample_pos(8, 5001)
+while min(list2)<500:
+    list2 = constrained_sum_sample_pos(8, 5001)
+print(list2)
