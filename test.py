27c486ec328c679b6a7c511abd004e158484a53e
aff49866 <z498660443@gmail.com>
2017-10-16 11:28:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -37,7 +37,7 @@
 # -*- coding: UTF-8 -*-
 import requests
 import re
-response = requests.get("http://www.baidu.com/")
+response = requests.get("http://www.baidu.comfdsffd/")
 response.encoding = response.apparent_encoding
 response = response.text
 
