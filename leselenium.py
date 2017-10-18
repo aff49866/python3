b7d09d2396b9a683db62540b01a75cfd8c02d647
aff49866 <z498660443@gmail.com>
2017-10-18 17:43:02 +0800

    微博登录

--- leselenium.py
+++ leselenium.py
@@ -1,12 +1,14 @@
 from selenium import webdriver  #导入Selenium的webdriver
 from selenium.webdriver.common.keys import Keys  #导入Keys
 
+account = input()
+password = input()
 driver = webdriver.Chrome()  #指定使用的浏览器，初始化webdriver
 driver.get("https://weibo.com/")  #请求网页地址
 driver.maximize_window()
 driver.implicitly_wait(5)
 user = driver.find_element_by_id("loginname")
 pwd = driver.find_element_by_name("password")
-user.send_keys("username")  #在搜索框中输入pycon
-pwd.send_keys("z")  #相当于回车键，提交
+user.send_keys(account)  #在搜索框中输入pycon
+pwd.send_keys(password)  #相当于回车键，提交
 driver.find_element_by_class_name("W_btn_a").click()
