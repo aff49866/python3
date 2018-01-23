4ede02fcd135c880015a459e87dbf88d76aa9c9b
aff49866 <z498660443@gmail.com>
2018-01-23 17:45:56 +0800

    .

--- zimeika.py
+++ zimeika.py
@@ -1,5 +1,6 @@
-# import requests
-# import re
+import requests
+import re
+import pymysql
 # def gethtml(url):
 #     try:
 #         headers = {
@@ -17,11 +18,71 @@
 #     except ZeroDivisionError as e:
 #         raise ValueError(e)
 # gethtml('http://zimeika.com/article/lists/tiantian.html?cate_id=1&time_type=&read_order=&type=&author_id=&author_name=&title=&p=1')
-import pymysql
-conn = pymysql.connect(host='211.149.173.153', port=3306, user='zhangmaode', passwd='zhang498660443',db='empirecms',charset='utf8')
-cur = conn.cursor()
-cur.execute("SELECT * FROM phome_ecms_news")
-for r in cur.fetchall():
-    print(r)
+# cur.execute("SELECT * FROM phome_ecms_news")
+# for r in cur.fetchall():
+#     print(r)
 #cur.close()
-conn.close()
+def getTianTianHtml(url):
+    try:
+        headers = {
+            'authority': 'kuaibao.qq.com',
+            'cookie': 'pgv_pvi=3894466560; RK=SIUOA7A6Td; pac_uid=1_155220296; kb_h5_user_id=KBH5UserId_151245445549256; tvfe_boss_uuid=9111743a133a0174; pgv_pvid=14073895; o_cookie=155220296; pgv_si=s4167611392; _qpsvr_localtk=0.19427851323596435; ptisp=ctc; ptcz=5e93651808285adca548c3c84d1b674c33256a4f48a68d0df89bfaa3eb95c831; uin=o0155220296; skey=@KB2aLQS0T; pt2gguin=o0155220296',
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+        }
+        r = requests.get(url,headers=headers)
+        r.raise_for_status()
+        r.encoding = 'utf-8'
+        return r.text
+    except ZeroDivisionError as e:
+        raise ValueError(e)
+def getInfo(html):
+    try:
+        regTitle = re.compile(r"'title': '(.*?)'", re.S)
+        regContent = re.compile(r'<div class="content-box">(.*?)</div>', re.S)
+        contentTxt ={}
+        contentTxt['title'] = regTitle.findall(html)
+        contentTxt['content'] = regContent.findall(html)
+        # contentStr = contentTxt['content']
+        # contentTxt['content'] = re.sub(r'本文(.*?)原创','',str(contentStr))
+        return contentTxt
+        # title.append(reg.findall(html)[0])
+        # if reg2.findall(html):
+        #     imagesmore = reg2.findall(html)
+        # elif reg2.findall(html):
+        #     imagesmore = reg3.findall(html)
+        # else:
+        #     imagesmore = reg4.findall(html)
+        # imageslist = sorted(set(imagesmore), key=imagesmore.index)
+        # for i in imageslist:
+        #     imageurl = "http://p1.pstatp.com/origin/" + i
+        #     infolist.append(imageurl)
+    except ZeroDivisionError as e:
+        raise ValueError(e)
+def connectdb():
+    db = pymysql.connect(host='211.149.173.153', port=3306, user='zhangmaode', passwd='zhang498660443',db='empirecms', charset='utf8')
+    return db
+def insertdb(db,contentTxt):
+    cursor = db.cursor()
+    sql = """INSERT INTO phome_ecms_news (classid,title)
+             VALUES ('1','contentTxt["title"]')"""
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        print('插入数据失败!')
+        db.rollback()
+def closedb(db):
+    db.close()
+def main():
+    # 抓取html
+    html = getTianTianHtml('https://kuaibao.qq.com/s/20180122A107UE00')
+    contentTxt = getInfo(html)
+    # 连接数据库，插入数据
+    db = connectdb()
+    insertdb(db,contentTxt)
+    closedb(db)
+if __name__ == '__main__':
+    main()
